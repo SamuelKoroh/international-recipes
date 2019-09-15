7e4e2e9c434850cfcd67728e10e3a74a2bab8d94
Sam <samuelkoroh@gmail.com>
2019-09-15 16:37:29 +0100

    finish setting webpack

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import 'font-awesome/css/font-awesome.css';
+import '../styles/app.scss';
+
+const App = () => (
+  <div>
+    <button type="button" className="btn btn-primary">Hello</button>
+  </div>
+);
+
+export default App;
